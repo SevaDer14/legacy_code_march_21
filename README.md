2eb9b8d32529223f8ceaaf26e7d0559289a597a5
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-21 14:03:25 +0200

    comments the app code for readability

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 Your challenge is to the test the existing code base and potentially add new features. 
 Make sure to follow the AUT cycle when writing your tests.  
+
+
+# Getting to Know Legacy code
+1. - it uses Monkeylearn to analyse text for moderation
+2. - and Clarifai to analyse pictures for moderation
--- app/controllers/api/analyses_controller.rb
+++ app/controllers/api/analyses_controller.rb
@@ -1,25 +1,37 @@
 # frozen_string_literal: true
 
 class Api::AnalysesController < ApplicationController
+  
+  #??
   before_action :analyze_resource, only: [:create]
 
   def create
+    # creates db migration Analysis?? with
+    # data from external AI APIs
     analysis = Analysis.create(analysis_params
                                    .merge!(results: @results,
                                            request_ip: request.remote_ip))
+    
+    # API output
+    # condition to handle errors if there is no responce from AI APIs
     if analysis.persisted?
       render json: analysis
     else
       render json: analysis.errors.full_messages, status: 422
     end
+
   end
 
   private
 
+  #takes :analysis data from params, permits all fields (better to reduce scope)
+  #used as input to new Analysis class
   def analysis_params
     params.require(:analysis).permit!
   end
 
+  # cheks what we selected to analyse, image or text and
+  # chooses corresponding AI API
   def analyze_resource
     resource = analysis_params[:resource]
     if analysis_category == :image
@@ -29,12 +41,14 @@ class Api::AnalysesController < ApplicationController
     end
   end
 
+  # sends data to Monkeylearn AI API (text analysis)
   def text_analysis(text)
     model_id = 'cl_KFXhoTdt' # Profanity & Abuse Detection
     response = Monkeylearn.classifiers.classify(model_id, [text])
     response.body[0]
   end
 
+  # sends data to Clarifai AI API (image analysis)
   def image_analysis(url)
     Clarifai::Rails::Detector
       .new(url)
@@ -42,6 +56,7 @@ class Api::AnalysesController < ApplicationController
       .concepts_with_percent
   end
 
+  # takes category from params and converts to symbol 
   def analysis_category
     analysis_params[:category].to_sym
   end
