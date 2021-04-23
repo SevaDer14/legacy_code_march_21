286bea1af2a44b44f213a81fc58ddc906e559e8f
DavDan1 <david-danielyan@hotmail.com>
2021-04-23 12:57:47 +0200

    wip stabs monkeylearn and adds moderation_results.json

--- spec/requests/api/moderation_api_responds_with_text_analysis_spec.rb
+++ spec/requests/api/moderation_api_responds_with_text_analysis_spec.rb
@@ -18,8 +18,8 @@ RSpec.describe 'POST /api/analyses', type: :request do
     }
 
     before do        
-      #stub_request(:post, "https://api.clarifai.com/v2/models/d16f390eb32cad478c7ae150069bd2c6/outputs")
-      #.to_return(status: 200, body: clarifai_responce )
+      stub_request(:post, "https://api.monkeylearn.com/v3/classifiers/cl_KFXhoTdt/classify/")
+      .to_return(status: 200, body: monkeylearn_responce)
       post '/api/analyses', params: params 
     end
 
